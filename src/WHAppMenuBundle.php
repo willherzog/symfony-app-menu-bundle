db0614346393fc04504642d348e59574c56656ea
Will Herzog <willherzog@gmail.com>
2025-06-27 14:30:23 -0600

    Register bundle's Twig extension as a service

--- src/WHAppMenuBundle.php
+++ src/WHAppMenuBundle.php
@@ -5,10 +5,12 @@ namespace WHSymfony\WHAppMenuBundle;
 use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 use WHSymfony\WHAppMenuBundle\DependencyInjection\AddAppMenuPass;
 use WHSymfony\WHAppMenuBundle\Menu\AppMenu;
+use WHSymfony\WHAppMenuBundle\Twig\WHAppMenuExtension;
 
 /**
  * @author Will Herzog <willherzog@gmail.com>
@@ -24,6 +26,15 @@ class WHAppMenuBundle extends AbstractBundle
 
 	public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
 	{
-		$builder->registerForAutoconfiguration(AppMenu::class)->addTag('wh_app_menu.menu');
+		$builder
+			->registerForAutoconfiguration(AppMenu::class)
+				->addTag('wh_app_menu.menu')
+		;
+
+		$container->services()
+			->set('wh_app_menu.twig.extension', WHAppMenuExtension::class)
+				->args([service('request_stack'), service('wh_app_menu.menu_loader')])
+				->tag('twig.extension')
+		;
 	}
 }
